e1d7e75112fee3a76cbf504b12d97c069abf4577
Matthew Landauer <matthew@oaf.org.au>
2015-03-16 12:42:03 +1100

    once again

--- scraper.rb
+++ scraper.rb
@@ -3,7 +3,7 @@ require 'scraperwiki'
 puts "Running scraper"
 ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer", "time" => Time.now})
 puts "Sleeping for 5 seconds"
-1..5.each do |i|
+(1..5).each do |i|
   puts "#{i}..."
   sleep 1
 end
