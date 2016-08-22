1cc60ac37b05d6a7e41935f1be7a1633024f1dad
Matthew Landauer <mlandauer@gmail.com>
2016-08-23 09:47:58 +1000

    Temporarily comment out network test

--- scraper.rb
+++ scraper.rb
@@ -6,9 +6,9 @@ puts "Running scraper"
 ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer", "time" => Time.now})
 # Check that mitmproxy gives a working certificate for SSL connections
 # If the certificate isn't valid it should throw an exception
-open("https://morph.io") do |f|
-  raise "Unexpected result" unless f.read.include?("Hassle-free web scraping.")
-end
+# open("https://morph.io") do |f|
+#   raise "Unexpected result" unless f.read.include?("Hassle-free web scraping.")
+# end
 # Check that output streaming works (in which case you should see each line one second apart
 # rather than getting all the lines at the end)
 puts "Sleeping for 5 seconds"
